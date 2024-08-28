89d27c5e2ff61a693bc9f254cbe875d5860b38aa
Leo-GG <leonardo.garma@gmail.com>
2024-08-28 15:46:20 +0200

    Add files via upload

---
+++ PrimerUtils/utils.py
@@ -0,0 +1,108 @@
+import requests
+import sys
+from Bio.SeqUtils import MeltingTemp as mt
+from Bio.Seq import Seq
+from Bio import pairwise2
+from Bio.pairwise2 import format_alignment
+from bs4 import BeautifulSoup
+
+
+def get_genome_sequence(chromosome, start, end):
+    server = "https://rest.ensembl.org"
+    ext = f"/sequence/region/human/{chromosome}:{start}..{end}:1"
+    headers = {"Content-Type": "application/json"}
+ 
+    
+    response = requests.get(server + ext, headers=headers)
+    # Raise an HTTPError for bad responses (4xx and 5xx)
+    response.raise_for_status()
+
+
+    #if not response.ok:
+    #    response.raise_for_status()
+    #    sys.exit()
+    return response.json()    
+    
+
+
+def design_primers(sequence, primer_length=20, target_tm=60, min_gc=40, max_gc=60, max_iterations=10):
+    forward_primer = sequence[0:primer_length]
+    reverse_primer =sequence[len(sequence)-primer_length:len(sequence)]
+ 
+    # Get reverse complement for the reverse primer
+    reverse_complement = str(Seq(reverse_primer).reverse_complement())
+    
+    # Adjust primer length to achieve target Tm (with a maximum number of iterations)
+    for _ in range(max_iterations):
+        f_tm = calculate_tm(forward_primer)
+        r_tm = calculate_tm(reverse_complement)
+        
+        if abs(f_tm - target_tm) <= 2 and abs(r_tm - target_tm) <= 2:
+            break
+        
+        if f_tm < target_tm and len(forward_primer) < 30:
+            forward_primer += sequence[len(forward_primer)]
+        elif f_tm > target_tm and len(forward_primer) > 18:
+            forward_primer = forward_primer[:-1]
+       
+        if r_tm < target_tm and len(reverse_complement) < 30:
+            reverse_complement =  reverse_complement + str(Seq(sequence[-len(reverse_complement)-1]).complement())
+        elif r_tm > target_tm and len(reverse_complement) > 18:
+            reverse_complement = reverse_complement[1:]
+    
+    # Check GC content (40-60% is ideal)
+    forward_gc = (forward_primer.count('G') + forward_primer.count('C')) / len(forward_primer) * 100
+    reverse_gc = (reverse_complement.count('G') + reverse_complement.count('C')) / len(reverse_complement) * 100
+    
+    if min_gc <= forward_gc <= max_gc and min_gc <= reverse_gc <= max_gc:
+        return forward_primer, reverse_complement, f_tm, r_tm
+    else:
+        raise ValueError(f"GC content is out of the ideal range ({min_gc}-{max_gc}%)")
+        
+def calculate_tm(primer):
+    return mt.Tm_NN(primer, nn_table=mt.DNA_NN4)
+
+
+def run_in_silico_pcr(forward_primer, reverse_primer, genome_assembly='hg38'):
+    url = 'https://genome.ucsc.edu/cgi-bin/hgPcr'
+    payload = {
+        'org': 'Human',
+        'db': 'hg38',
+        'wp_target': 'genome',
+        'wp_f': forward_primer,
+        'wp_r': reverse_primer,
+        'wp_size': '4000',
+        'Submit': 'Submit',
+        'wp_perfect':'5',
+        'wp_good':'15',
+        'boolshad.wp_flipReverse':'0',
+        'wp_append':'on',
+        'boolshad.wp_append':'0'             
+    }
+    
+    response = requests.post(url, data=payload)
+    
+    response.raise_for_status()
+
+    if response.status_code == 200:
+        return extract_alignments(response.text)[0]
+    else:
+        return {'Error':response.status_code}
+    
+
+def extract_alignments(html_text):
+    soup = BeautifulSoup(html_text, 'html.parser')
+    alignments = []
+
+    for pre in soup.find_all('pre'):
+        alignment_info = pre.text.strip().split('\n')
+        if len(alignment_info) > 1:
+            alignments.append({
+                'location': alignment_info[0].split()[0],
+                'length': alignment_info[0].split()[1].replace('bp', ''),
+                'forward_primer': alignment_info[0].split()[2],
+                'reverse_primer': alignment_info[0].split()[3],
+                'sequence': '\n'.join(alignment_info[1:])
+            })
+
+    return alignments
---
+++ app.py
@@ -0,0 +1,148 @@
+import streamlit as st
+from PrimerUtils import utils
+import requests
+
+# Function to run with user input
+def process_inputs(chromosome, target_position, region_length=100, primer_length=20, target_tm=60, min_gc=40, max_gc=60):
+    
+    start= target_position - region_length // 2 - primer_length
+    end = target_position + region_length // 2 + primer_length
+    
+    try:
+        ensembl_response = utils.get_genome_sequence(chromosome, start, end)
+        sequence=ensembl_response['seq']
+        build=ensembl_response['id'].split(':')[1]
+
+        result={ 
+            'Build':build,
+            'Sequence':sequence
+        }
+    except requests.exceptions.HTTPError as e:
+        # Handle the HTTPError separately
+        #print(f"HTTPError: {e}")
+        result={'HTTPError': e}
+        return result
+        
+    
+    except requests.exceptions.RequestException as e:
+        # Handle other possible exceptions related to the request (e.g., ConnectionError)
+        #print(f"RequestException: {e}")
+        result={'RequestException': e}
+        return result
+    
+    try:
+        
+        forward_primer, reverse_primer, forward_tm, reverse_tm = utils.design_primers(sequence,
+                                                                                      primer_length=primer_length,
+                                                                                      target_tm=target_tm, 
+                                                                                      min_gc=min_gc, 
+                                                                                      max_gc=max_gc)
+
+        result['Forward primer']=forward_primer
+        result['Reverse primer']=reverse_primer
+        result['Forward primer Tm']=f"{forward_tm:.2f}°C"
+        result['Reverse primer Tm']=f"{reverse_tm:.2f}°C"
+        
+        try:
+            result['Blast results']=utils.run_in_silico_pcr(forward_primer,reverse_primer)
+            
+        except requests.exceptions.HTTPError as e:
+            # Handle the HTTPError separately
+            #print(f"HTTPError: {e}")
+            result['Blast HTTPError']=e
+            return result   
+        except requests.exceptions.RequestException as e:
+            # Handle other possible exceptions related to the request (e.g., ConnectionError)
+            #print(f"RequestException: {e}")
+            result['Blast RequestException']=e
+            return result
+        
+        return result
+
+    except ValueError as e:
+        
+        result['Error'] = str(e)
+        
+        return result
+
+# Streamlit app
+def main():
+    st.title("Quick primer design")
+    description = r""" This application creates PCR primers to amplify the sequence around a selected position in the **human** genome. It returns the 
+    pair of forward and reversed primers and then uses the [UCSC In-Silico PCR tool](https://genome.ucsc.edu/cgi-bin/hgPcr) to check for
+    alignments along the genome.
+    
+    Tm are calculated using the [MeltingTemp](https://biopython.org/docs/1.75/api/Bio.SeqUtils.MeltingTemp.html) module from the [Biopython](https://biopython.org/) package.
+    """
+    st.markdown(description, unsafe_allow_html=True)
+
+
+    # Sidebar (left panel)
+    st.sidebar.title("Input Panel")
+
+    # Dropdown for choosing chromosome
+    chromosome_list=list(['CHRM '+str(i) for i in range(1,23)])+list(['CHRM X','CHRM Y'])
+    chromosome = st.sidebar.selectbox(
+        "Choose Chromosome",
+        chromosome_list,0
+    ).split('CHRM ')[1]
+        
+    
+    # Input box for position (with error handling for numerical input)
+    position = st.sidebar.text_input("Enter Position",1)
+    try:
+        position = int(position)
+    except ValueError:
+        st.sidebar.error("Position must be a number")
+        position = None
+    
+    # Radio buttons to choose organism
+    organism = st.sidebar.radio(
+        "Choose Organism",
+        (["Human"])
+    )
+    
+    # Additional numerical inputs for extra parameters
+    region_length = st.sidebar.number_input("Target region length", value=100)
+    primer_length = st.sidebar.number_input("Initial primer length", value=20)
+    target_tm = st.sidebar.number_input("Ideal Tm", value=55)
+    min_gc = st.sidebar.number_input("Minimum %GC", value=20)
+    max_gc = st.sidebar.number_input("Maximum %GC", value=80)
+    
+    # Run button
+    run_button = st.sidebar.button("Run")
+    
+    # Main page
+    
+
+    if run_button:
+        st.title("Results")
+        # Only run if position is a valid number
+        result = process_inputs(chromosome, position, region_length, primer_length, target_tm, min_gc, max_gc)
+        if position is not None:
+            try:
+                # Call the processing function with the selected parameters
+                result = process_inputs(chromosome, position, region_length, primer_length, target_tm, min_gc, max_gc)
+                # Display the results
+                st.markdown("### Primers")
+                for key, value in result.items():
+                    if ('Blast' not in key):
+                        st.markdown(f"**{key}:** {value}")
+                    else:
+                        blast_output=value
+
+                st.markdown("### Blast search")
+                for key, value in blast_output.items():
+                    st.markdown(f"**{key}:** {value}")
+
+                
+            except:
+                st.write(result)
+                
+        else:
+            st.write("Please enter a valid position.")
+    else:
+        st.title("Press Run to design your primers")
+    
+if __name__ == "__main__":
+    main()
